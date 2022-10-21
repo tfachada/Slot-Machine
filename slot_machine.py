ea032a6abf3f47f13e8ff23bb52dd93ce6b4ce09
Tomás Fachada <tom.fachada@gmail.com>
2022-10-21 12:50:07 +0100

    code importable, also string cleanups

--- slot_machine.py
+++ slot_machine.py
@@ -23,43 +23,26 @@ class Player:
     self.credits += machine.payoffs[symbol] * current_bet
 
 
-### INITIAL INTERFACE ###
-# Player sets a positive integer number of credits.
-# After the initial credits are defined, the game itself can start.
+### AUXILIARY FUNCTIONS ###
+# set_credits(), which sets a positive integer initial number of credits the player starts with
+# set_bet(), which does the same for each bet, plus check if it doesn't exceed the credits
 
-print("\n\t\t\t\tSLOT MACHINE\n")
-
-while True:
-  try:
-    initial_credits = int(input("How much have you got?\n"))
-    if initial_credits <= 0:
-      print("Please enter a positive number\n")
-    else:
-      break
-  except ValueError:
-    print("Please input an integer\n")
-
-print("\n--------------------\n")
-
-
-### GLOBAL VARIABLES FOR USE IN THE GAME ###
-# Init machine and player objects
-machine = Machine()
-player = Player(initial_credits)
-
-# Y/N flag for the main routine of the game
-keep_playing = ""
-
-
-### MAIN ROUTINE ###
-while player.credits > 0 and keep_playing != "N":
-
-  # reset flags for new iteration
-  invalid_bet = True
-  keep_playing = ""
+def set_credits():
+  while True:
+    try:
+      initial_credits = int(input("How much have you got?\n"))
+      if initial_credits <= 0:
+        print("Please enter a positive number\n")
+      else:
+        break
+    except ValueError:
+      print("Please enter an integer\n")
   
-  # bet is only valid if it's a positive integer and the player has the money
-  while invalid_bet:
+  return initial_credits
+
+
+def set_bet():
+  while True:
     try:
       current_bet = int(input("Place your bet: "))
       if current_bet <= 0:
@@ -67,38 +50,74 @@ while player.credits > 0 and keep_playing != "N":
       elif current_bet > player.credits:
         print("You don't have that much\n")
       else:
-        invalid_bet = False
+        break
     except ValueError:
-      print("Please input an integer\n")
+      print("Please enter an integer\n")
   
-  # place bet and get combination
-  player.bet(current_bet)
-  combination = machine.roll()
-
-  # then print it
-  print("\n")
-  print("-------------")
-  print("| " + combination[0] + " | " + combination[1] + " | " + combination[2] + " |")
-  print("-------------")
-  print("\n")
-
-  # and win if there's something to win
-  if combination[0] == combination[1] == combination[2]:
-    player.win(machine, combination[0], current_bet)
-  
-  print("Current credits: " + str(player.credits) + "\n")
-
-  # if the player can still play, ask if they want to, and set the keep_playing flag
-  if player.credits > 0:
-    while(keep_playing != "Y" and keep_playing != "N"):
-      keep_playing = input("Keep playing? (Y/N): ").upper()
+  return current_bet
 
 
-### GAME OVER ###
-if player.credits == 0:
-  print("You're broke. Bye!\n")
-else:
-  print("\n")
-  print("Started with:\t" + str(initial_credits))
-  print("Ended with:\t" + str(player.credits))
-  print("\n")
+### GAME ITSELF ###
+
+if __name__ == "__main__":
+
+  ## INITIAL INTERFACE ##
+  # Player sets a positive integer number of credits.
+  # After the initial credits are defined, the game itself can start.
+
+  print("\n\t\t\t\tSLOT MACHINE\n")
+
+  initial_credits = set_credits()
+
+  print("\n--------------------\n")
+
+
+  ## GLOBAL VARIABLES FOR USE IN THE GAME ##
+  # Init machine and player objects
+  machine = Machine()
+  player = Player(initial_credits)
+
+  # Y/N flag for the main routine of the game
+  keep_playing = ""
+
+
+  ## MAIN ROUTINE ##
+  while player.credits > 0 and keep_playing != "N":
+
+    # reset flag for new iteration
+    keep_playing = ""
+    
+    # ask player for the bet
+    current_bet = set_bet()
+    
+    # place bet and get combination
+    player.bet(current_bet)
+    combination = machine.roll()
+
+    # then print it
+    print("\n")
+    print("-------------")
+    print(f"| {combination[0]} | {combination[1]} | {combination[2]} |")
+    print("-------------")
+    print("\n")
+
+    # and win if there's something to win
+    if combination[0] == combination[1] == combination[2]:
+      player.win(machine, combination[0], current_bet)
+    
+    print("Current credits: " + str(player.credits) + "\n")
+
+    # if the player can still play, ask if they want to, and set the keep_playing flag
+    if player.credits > 0:
+      while(keep_playing != "Y" and keep_playing != "N"):
+        keep_playing = input("Keep playing? (Y/N): ").upper()
+
+
+  ## GAME OVER ##
+  if player.credits == 0:
+    print("You're broke. Bye!\n")
+  else:
+    print("\n")
+    print("Started with:\t" + str(initial_credits))
+    print("Ended with:\t" + str(player.credits))
+    print("\n")
